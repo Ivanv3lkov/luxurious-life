3f3d254c30aa1ea39e075cf8a69e8a768a13deee
Ivan Velkov <ivanv3lkov@gmail.com>
2023-04-09 18:25:43 +0300

    Add index.css file for some of the main css styles

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,6 +6,8 @@ import { persistor, store } from './store';
 
 import App from './App';
 
+import './index.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
