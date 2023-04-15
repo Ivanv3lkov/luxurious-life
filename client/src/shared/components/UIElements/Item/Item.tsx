9a8e7695259de4d4281f16d5f153003f8ebd18bb
Ivan Velkov <ivanv3lkov@gmail.com>
2023-04-15 12:28:04 +0300

    Make reusable Item and ItemList components

--- client/src/shared/components/UIElements/Item/Item.tsx
+++ client/src/shared/components/UIElements/Item/Item.tsx
@@ -24,31 +24,26 @@ export type Props = {
     hearts: string[];
     diamonds: string[];
   };
-  collectionName: string
+  collectionName: string;
 };
 
-const Item: React.FC<Props> = ({
-  id,
-  title,
-  model,
-  image,
-  reactions,
-  collectionName
-}) => {
+const Item: React.FC<Props> = ({ id, title, model, image, reactions, collectionName }) => {
   const { accessToken, userId } = useSelector((state: StoreState) => state.user);
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
   const [{ likes, hearts, diamonds }, setCarReactions] = useState(reactions);
   const isHomeLiked = userId ? likes.includes(userId) : false;
   const isHomeLoved = userId ? hearts.includes(userId) : false;
   const isHomePriceless = userId ? diamonds.includes(userId) : false;
-
+  
   const reactToHomeHandler = async (buttonText: string) => {
+    console.log(collectionName);
+
     try {
       const responseData = await sendRequest(
         `${process.env.REACT_APP_BACKEND_URL}/${collectionName}/${id}/reactions`,
         'PATCH',
         JSON.stringify({
-          homeId: id,
+          [collectionName === 'homes' ? 'homeId' : 'carId']: id,
           currentReaction: buttonText
         }),
         {
@@ -66,52 +61,52 @@ const Item: React.FC<Props> = ({
       <ErrorModal error={error} onClear={clearError} />
       {isLoading && <LoadingSpinner asOverlay />}
       <div className="item">
-          <div className="item__image">
-            <img src={`${process.env.REACT_APP_ASSET_URL}/${image}`} alt="img" />
+        <div className="item__image">
+          <img src={`${process.env.REACT_APP_ASSET_URL}/${image}`} alt="img" />
+        </div>
+        <div className="item__info">
+          <h2>{title ? title : model}</h2>
+          <div className="item__reactions">
+            <h3>
+              <FaThumbsUp size={22} />
+              {likes.length}
+            </h3>
+            <h3>
+              <FcLike size={22} />
+              {hearts.length}
+            </h3>
+            <h3>
+              <IoDiamondSharp size={21} />
+              {diamonds.length}
+            </h3>
           </div>
-          <div className="item__info">
-            <h2>{title ? title : model}</h2>
-            <div className="item__reactions">
-              <h3>
-                <FaThumbsUp size={22} />
-                {likes.length}
-              </h3>
-              <h3>
-                <FcLike size={22} />
-                {hearts.length}
-              </h3>
-              <h3>
-                <IoDiamondSharp size={21} />
-                {diamonds.length}
-              </h3>
-            </div>
+        </div>
+        <div className="item__actions">
+          <div>
+            <Button
+              onClick={(event: any) => reactToHomeHandler(event.currentTarget.textContent)}
+              disabled={!userId}
+            >
+              <FaThumbsUp size={15} transform={isHomeLiked ? 'scale(1 -1)' : ''} />
+              {isHomeLiked ? 'Unlike' : 'Like'}
+            </Button>
+            <Button
+              onClick={(event: any) => reactToHomeHandler(event.currentTarget.textContent)}
+              disabled={!userId}
+            >
+              {isHomeLoved ? <FcDislike size={15} /> : <FcLike size={15} />}
+              {isHomeLoved ? 'Unlove' : 'Love'}
+            </Button>
+            <Button
+              onClick={(event: any) => reactToHomeHandler(event.currentTarget.textContent)}
+              disabled={!userId}
+            >
+              {isHomePriceless ? <GiDiamondHard size={15} /> : <IoDiamondSharp size={15} />}
+              {isHomePriceless ? 'Worthless' : 'Priceless'}
+            </Button>
+            <Button to={`/${collectionName}/${id}/details`}>DETAILS</Button>
           </div>
-          <div className="item__actions">
-            <div>
-              <Button
-                onClick={(event: any) => reactToHomeHandler(event.currentTarget.textContent)}
-                disabled={!userId}
-              >
-                <FaThumbsUp size={15} transform={isHomeLiked ? 'scale(1 -1)' : ''} />
-                {isHomeLiked ? 'Unlike' : 'Like'}
-              </Button>
-              <Button
-                onClick={(event: any) => reactToHomeHandler(event.currentTarget.textContent)}
-                disabled={!userId}
-              >
-                {isHomeLoved ? <FcDislike size={15} /> : <FcLike size={15} />}
-                {isHomeLoved ? 'Unlove' : 'Love'}
-              </Button>
-              <Button
-                onClick={(event: any) => reactToHomeHandler(event.currentTarget.textContent)}
-                disabled={!userId}
-              >
-                {isHomePriceless ? <GiDiamondHard size={15} /> : <IoDiamondSharp size={15} />}
-                {isHomePriceless ? 'Worthless' : 'Priceless'}
-              </Button>
-              <Button to={`/${collectionName}/${id}/details`}>DETAILS</Button>
-            </div>
-          </div>
+        </div>
       </div>
     </>
   );
